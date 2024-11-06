5c8ddabdda451ca676e80e9e4773c73d38a89265
thakrani <thakranishivam@gmail.com>
2024-11-06 22:41:59 +0100

    feat: added device Model

--- models/models.go
+++ models/models.go
@@ -1,1 +1,10 @@
 package models
+
+import "time"
+
+type Device struct {
+	ID          int       `json:"id"`           // Unique identifier for the device
+	DeviceName  string    `json:"device_name"`  // Name of the device
+	DeviceBrand string    `json:"device_brand"` // Brand of the device
+	CreatedAt   time.Time `json:"created_at"`   // Timestamp of creation
+}
